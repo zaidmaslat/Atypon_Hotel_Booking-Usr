16827db9bb6cc5cda6c3a632bf76738259ce55fb
zaidmaslat <zaidmaslat.zm@gmail.com>
2023-03-07 23:59:32 +0300

    push changes for css users

--- src/Users/makeReservation.php
+++ src/Users/makeReservation.php
@@ -1,6 +1,48 @@
-<?php
-echo "<h1 style=\"text-align:center; font-size:50px;\">Make Reservation</h1>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/makeReservation.css">
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light paddingHeder ">
+			<a class="navbar-brand" href="#">Booking Hotel</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+				aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="/Users/usersLoginPage.php?userID=<?php echo $_GET['userID'] ?>">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="searchHotel.php?userID=<?php echo $_GET['userID'] ?>">Search for a hotel</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="monitorReservations.php?userID=<?php echo $_GET['userID'] ?>">Reservations</a>
+					</li>
+                    <li class="nav-item">
+						<a class="nav-link" href="#">Sign out</a>
+					</li>
+				</ul>
+			</div> 
+		</nav>
+    </header>
+    <div>
+        <h1 class="header">Make Reservation</h1>"
+    </div>
+    <div class="registration-form">
+
 <?php
 if (isset($_GET['notAvailable'])) {
     echo '<h3 style="color:red">Number of rooms requested are not available</h3>';
@@ -8,9 +50,13 @@ if (isset($_GET['notAvailable'])) {
 
 echo "<h1 style=\"text-align:left; font-size:20px;\">Please choose the hotel name:</h1>";
 ?>
-<form action="/Users/reserve.php" method="POST">
-    <input type="hidden" name="userID" value="<?php echo $_GET['userID'] ?>" >
-    <select name="hotelID">
+
+        <form action="/Users/reserve.php" method="POST">
+            <input type="hidden" name="userID" value="<?php echo $_GET['userID'] ?>">
+
+            <div class="form-group displayField arrow">
+                <label class="marginLabel">Hotel</label>
+                <select name="hotelID" class="form-control item">
         <?php
         $con = mysqli_connect("my-mysql.default.svc.cluster.local", "dbuser", "123456", "hotel_management");
         if (mysqli_connect_errno()) {
@@ -22,14 +68,24 @@ echo "<h1 style=\"text-align:left; font-size:20px;\">Please choose the hotel nam
 
         while ($row = mysqli_fetch_array($result)) {
             echo "<option value=" . $row['hotelID'] . ">"  . $row['hotelName'] .  "</option>";
-        }
+        } 
         ?>
-    </select>
-    <?php
-    echo "<h1 style=\"text-align:left; font-size:20px;\">Please choose the number of rooms:</h1>";
-    ?>
-    number-of-rooms:<br>
-    <input type="number" name="num-rooms" value=""><br>
+                    </select>
+            </div>
 
-    <input type="submit" value="Submit">
-</form>
+
+
+            <div class="form-group displayField">
+                <label class="marginLabel">Number of rooms</label>
+                <input type="number" name="num-rooms" class="form-control">
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary create-account">Submit</button>
+                </div>
+            </div>
+
+        </form>
+    </div>
+
+</body>
+
+</html>
--- src/Users/usersLoginPage.php
+++ src/Users/usersLoginPage.php
@@ -1,13 +1,83 @@
-<?php
-        $userID = $_GET['userID'];
+<!DOCTYPE html>
+<html lang="en">
 
-?>
-<?php
-echo "<h1 style=\"text-align:center; font-size:50px;\">Users Page</h1>";
-?>
-<?php
-echo "<h1 style='color:blue; text-align:center; font-size:25px;'><a href='searchHotel.php?userID=$userID'>Search for a Hotel</a></h1>";
-?>
-<?php
-echo "<h1 style='color:blue; text-align:center; font-size:25px;'><a href='monitorReservations.php?userID=$userID'>Monitor My Reservations</a></h1>";
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/usersLoginPage.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light paddingHeder ">
+			<a class="navbar-brand" href="#">Booking Hotel</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+				aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="/Users/usersLoginPage.php?userID=<?php echo $_GET['userID'] ?>">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="searchHotel.php?userID=<?php echo $_GET['userID'];?>">Search for a hotel</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="monitorReservations.php?userID=<?php echo $_GET['userID'];?>">Reservations</a>
+					</li>
+                    <li class="nav-item">
+						<a class="nav-link" href="#">Sign out</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+    </header>
+    <Main >
+        <br><br><br><br><br>
+        <div class="section_our_solution">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <div class="our_solution_category">
+                        <div class="solution_cards_box">
+                            <div class="solution_card flexCard">
+                                <div class="hover_color_bubble"></div>
+                                <div class="solu_title">
+                                </div>
+                                <div class="solu_description">
+                                    <h3>
+                                        <a class="link" href='searchHotel.php?userID=<?php echo $_GET['userID'];?></a>'>Search for a Hotel</a>
+                                    </h3>
+                                </div>
+                            </div>
+                        
+                        </div>
+                        <div class="solution_cards_box sol_card_top_3">
+
+                            <div class="solution_card flexCard">
+                                <div class="hover_color_bubble"></div>
+                                
+                                <div class="solu_description">
+                                    <h3>
+
+                                        <a class="link" href='monitorReservations.php?userID=<?php echo $_GET['userID'];?></a>'>Monitor My Reservations</a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </Main>
+
+</body>
+
+</html>
